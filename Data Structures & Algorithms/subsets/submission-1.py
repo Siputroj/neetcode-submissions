83b9ab8fbdce5e66c900721cae869bc9cfb793ca
Jason Siputro <95631800+Siputroj@users.noreply.github.com>
2026-09-08 18:47:15 +0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        output = [[]]
+
+        for n in nums:
+            stage = []
+
+            for s in output:
+                new_s = s.copy()
+                new_s.append(n)
+                stage.append(new_s)
+
+            output.extend(stage)
+
+        return output
