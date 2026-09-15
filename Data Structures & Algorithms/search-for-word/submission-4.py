81ebb886dd581b2a29ff405ed38d8ae393da8007
Jason Siputro <95631800+Siputroj@users.noreply.github.com>
2026-09-15 07:04:01 +0700

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.py
@@ -0,0 +1,45 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        # do a for loop to go through all the words
+        # if the first word is found do a bfs
+        def dfs(i, row, col, visited):
+            
+            if i == len(word):
+                return True
+            
+            # check if still in board
+            if (row >= len(board) or col >= len(board[0])) or (row < 0 or col < 0) :
+                return False
+            
+            # check if visited
+            if (row, col) in visited:
+                return False
+
+            # check if word is good
+            if board[row][col] != word[i]:
+                return False
+
+            visited.add((row, col))
+
+            # Explore
+            found = (
+                dfs(i + 1, row + 1, col, visited)
+                or dfs(i + 1, row, col + 1, visited)
+                or dfs(i + 1, row - 1, col, visited)
+                or dfs(i + 1, row, col - 1, visited)
+            )
+
+            # Backtrack (un-choose)
+            visited.remove((row, col))
+
+            return found
+
+        res = False
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == word[0]:
+                    res = dfs(0, row, col, set())
+                if res:
+                    return True
+
+        return False
